760c87dd276e7c6d976c0cd69a0c0c755e21eede
TetianaMyslynska <tetmyslynska@gmail.com>
2021-11-08 21:01:55 +0100

    all done

--- js/app.js
+++ js/app.js
@@ -13,10 +13,9 @@ refs.galleryList.insertAdjacentHTML("beforeend", galleryMarkup);
 function createGalleryMarkup(elements) {
   return elements
     .map(({ preview, original, description }) => {
-      return;
-      `<li class = 'gallery__item'>
-      <a class = 'gallery__link' href = '${original}' >
-      <img class = 'gallery__image' src = '${preview}'data-source = '${original}' alt = '${description}'/></a></li>`;
+      return `<li class = 'gallery__item'>
+<a class = 'gallery__link' href = '${original}' >
+<img class = 'gallery__image' src = '${preview}'data-source = '${original}' alt = '${description}'/></a></li>`;
     })
     .join("");
 }
